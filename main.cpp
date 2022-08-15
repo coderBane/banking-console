e59c3b94cbf5ec410438f577b3edfb30bc5856d4
coder <110666273+coderBane@users.noreply.github.com>
2022-08-15 14:58:38 +0100

    modifications

--- main.cpp
+++ main.cpp
@@ -1,18 +1,17 @@
 #include <iostream>
 #include <bank.hpp>
 
-// #define getid(x) ((x).id)
+
 using namespace bank;
 
 int main(int, char**) {
 
-    SavingsAccount acct("John", "Maddy", 200);
+    Customer *c = new Customer;
 
-    acct.withdraw(250);
-    acct.deposit(1000);
-    acct.transactionHistory();
-    acct.deposit(-1);
-    
-    std::cout << getBalance(acct);
+    enroll(c);
 
+    c->customerInfo();
+
+    //std::cout << sizeof(c);
+    delete(c);
 }
