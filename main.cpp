921a13be3b0a8241f6b88f488cbbfdf9bacb61aa
coder <110666273+coderBane@users.noreply.github.com>
2022-11-10 08:40:17 +0000

    code execution attempt

--- main.cpp
+++ main.cpp
@@ -1,83 +1,45 @@
 #include <iostream>
-#include <bank.hpp>
+#include "bank.hpp"
+#include "utils.hpp"
+
 
 using namespace std;
 using namespace bank;
 
-typedef unsigned char uchar;
-void actions(uchar&, Account);
-
 int main(int, char**) {
 
-    Customer *c = new Customer();
-    uchar fp;
+    unique_ptr<Customer> customer(new Customer());
+    signUp(customer.get());
 
-    c->initialize();
-    cout << "Press Enter to continue..." << endl;
-    getchar();
+    cout << "Press any key to continue..." << endl;
+    auto choice = getchar();
 
-    if (getchar() == 0x0A){
-    
-        do
-        {
-            clcsc;
-            cout << "Choose account to access: (Key Press)" <<endl;
-            cout << "C Current\nS Savings\nI Summary\nQ Exit" <<endl;
-
-            fp = getchar();
-
-            switch(fp)
-            {
-                case 0x63:
-                    options();
-                    break;
-
-                case 0x69:
-                    c->customerInfo(); 
-                    break;
-                
-                case 0x73:
-                    options();
-                    break;
-
-                default: break;
-            }
-
-        }while(fp != 0x71);
-    }
-    
-    delete(c);
-}
-
-void actions(uchar &key, Account acct)
-{
-    double amt;
-    switch (cin.get())
+    do
     {
-        case 0x62:
-            cout << "Account Balance: " << getBalance(acct) <<endl;
-            break;
-        
-        case 0x64:
-            cout << "Enter Ammount > ";
-            cin >> amt;
-            acct.deposit(amt); 
-            break;
+        clearScreen;
+        cout << "\nChoose account to access: (Key Press)" << endl;
+        cout << "C Current\nS Savings\nI Summary\nQ Exit" << endl;
 
-        case 0x77:
-            cout << "Enter Amount" ;
-            cin >> amt;
-            acct.withdraw(amt);
-            break;
+        choice = getchar();
 
-        case 0x74:
-            acct.transactionHistory();
-            break;
+        if (choice == 99){
+            options();
+            choice = getchar();
+            actions(choice, customer->accounts.front());
+        }
+        else if (choice == 105)
+            customer->customerInfo();
 
-        case 0x69:
-            acct.accountInfo();
-            break;
+        else if (choice == 115){
+            options();
+            choice = getchar();
+            actions(choice, customer->accounts.back());
+        }
 
-        default: break;
-    }
+        continue;
+
+    }while(choice != 'q');
+
+    return 0;
 }
+
